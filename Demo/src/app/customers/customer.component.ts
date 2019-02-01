60c9b1ddf36ef660fa23f2a76bd23dc2b73a66db
Stephan Ramke <stephan.ramke@web.de>
2019-02-01 21:52:27 +0100

    use FormBuilder

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -12,14 +12,14 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: {value: '', disabled: false},
+      email: '',
+      sendCatalog: true
     });
   }
 
